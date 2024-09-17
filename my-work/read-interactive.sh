63c303b71a422fe3d12c60133deb22c1371f89cf
Cy-2004 <cy2004@Chelsea>
2024-09-17 12:13:48 -0400

    in class changes 9/17

---
+++ my-work/read-interactive.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p "What is the input file name? " INPUT
+read -p "What is the output file name? " OUTPUT
+
+echo "I will use the $INPUT filename to give you a new $OUTPUT"
+
---
+++ my-work/this.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Echo out something
+
+VAR1="Vanilla"
+
+$USER
+
+echo "Hey $USER I see that your favorite flavor of ice cream is $VAR1"
