7f8a5181f353fbe1cae3374d2476feeba95574bb
Cy-2004 <150986295+Cy-2004@users.noreply.github.com>
2024-09-19 18:27:35 -0400

    Update list-classmates.sh

--- my-work/list-classmates.sh
+++ my-work/list-classmates.sh
@@ -10,9 +10,9 @@ target_file="README.md"
 
 # loop through all dirs and find target file
 for dir in $( find . -type d ); do 
-  if[ -f "$dir/$target_file" ]; then
+  if [ -f "$dir/$target_file" ]; then
     #echo "$dir/$target_file";
-    name='head -n 1 "dir/$target_file"';
+    name=`head -n 1 "$dir/$target_file"`;
     echo $name;
   fi
 done
